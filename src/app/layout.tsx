bea64d144677dde96c0494b22c40cd905e1439c9
netrobase <netrobase@gmail.com>
2024-11-09 23:52:08 +0100

    Update Layout

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import "./globals.css";
+
+export const metadata = {
+  title: 'Crest View',
+  description: "This is a school website design.",
+  keywords: ["kralis", "school websites", "school website templates", "Netrobase"],
+  metadataBase: new URL("https://www.templates.kralis.app/"),
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className=""
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
